5dc58a89c73aba3e7930028af754018a3ac60021
Christian Schulze <accounts@andinfinity.de>
2023-03-05 23:03:54 +0100

    fix: added missing newline after PR listing

--- src/action.go
+++ src/action.go
@@ -65,10 +65,10 @@ func updateOrCreateDraftRelease(a *Action, cfg *config.RepoConfig) (*gitea.Relea
 				fmt.Fprintf(&b, "## %s\n\n", strings.Title(label))
 
 				for _, pr := range prs {
-					fmt.Fprintf(&b, "* %s (#%d) @%s", pr.Title, pr.ID, pr.Poster.UserName)
+					fmt.Fprintf(&b, "* %s (#%d) @%s\n", pr.Title, pr.ID, pr.Poster.UserName)
 				}
 
-				b.WriteString("\n\n")
+				b.WriteString("\n")
 			}
 		}
 	}
